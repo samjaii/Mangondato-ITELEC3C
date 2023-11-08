08328e64d03f69272663ddbf8a7aa5f3afe60589
JaiMngndto <123555594+JaiMngndto@users.noreply.github.com>
2023-11-08 14:05:01 +0800

    Complete

---
+++ resources/views/pages/users.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+
+@section('title')
+<title> Exercise: Users </title>
+@endsection
+
+@section('content')
+<h2 class="text-center"> Users </h2>
+<table class="table table-striped table-hover">
+    <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Name</th>
+            <th scope="col">Email</th>
+            <th scope="col">Created At</th>
+            <th scope="col">Updated At</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach($users as $user)
+        <tr>
+            <td>{{ $user->id }}</td>
+            <td>{{ $user->name }}</td>
+            <td>{{ $user->email }}</td>
+            <td>{{ $user->created_at}}</td>
+            <td>{{ $user->updated_at}}</td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/users', function () {
+    $users = User::all();
+
+    return view('pages.users', ['users' => $users]);
 });
